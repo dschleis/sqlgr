eab4b555edccd7a4202e2aa35a5781763b4b16b2
dschleis <dschleis@gmail.com>
2017-07-18 08:16:27 -0500

    dded additional grabSingleThing methods

--- sqlgr.groovy
+++ sqlgr.groovy
@@ -1,9 +1,9 @@
 /**********************************************************
-SqLgr
+Sqlgr
 Helper methods to make SQL Developer Data Modeler scripting easier
 
 Dave Schleis
-2017-06-17
+2017-Jul-07
 **********************************************************/
 /*
   grabTables()
@@ -17,11 +17,18 @@ def grabTables() {
   Get a list of table objects from the current relational model.
   If includeMe is not null then only those tables that contain includeMe are returned.
   If includeMe is a string containing comma-seperated values, tables containing any of those strings will be returned.
+  includeMe uses a double wildcard match by default. Adding a '*' or '%' to the beginning or end
+  of the string will match "ends with" and "begins with" respectivly.
 */
 def grabTablesWith(String includeMe='') {
   grabThings(model,'TABLE', includeMe)
 }
 
+/*
+  grabTableWith(String includeMe='')
+  This method will return a single table object that matches includeMe.
+  The method will return null if there are no matches or more than one match
+*/
 def grabTableWith(String includeMe='') {
   grabSingleThing(model,'TABLE', includeMe)
 }
@@ -41,55 +48,67 @@ def grabTablesWithout(String excludeMe='') {
   If excludeList is provided, columns with names containing any of the strings
   in the excludeList will not be returned.
 */
-def grabColumns(oracle.dbtools.crest.model.design.relational.Table table) {
+def grabColumns(table) {
   grabColumnsWith(table)
 }
 
-def grabColumnsWith(oracle.dbtools.crest.model.design.relational.Table table, String includeMe='') {
+def grabColumnsWith(table, includeMe='') {
   grabThings(table,'COLUMN', includeMe)
 }
 
-def grabColumnsWithout(oracle.dbtools.crest.model.design.relational.Table table, String excludeMe='') {
+def grabColumnWith(table, includeMe='') {
+  grabSingleThing(table,'COLUMN', includeMe)
+}
+
+def grabColumnsWithout(table, excludeMe='') {
   grabThings(table,'COLUMN', excludeMe, true)
-}
-
-def grabColumnWith(oracle.dbtools.crest.model.design.relational.Table table, String includeMe='') {
-  def retVal = grabSingleThing(table,'COLUMN', includeMe)
-}
-
-
-def grabEntitiesWith(String includeMe='') {
-  grabThings(model,'ENTITY', includeMe)
-}
-
-def grabEntities() {
-  grabThings(model,'ENTITY')
-}
-
-def grabEntitiesWithout(String excludeMe='') {
-  grabThings(model,'ENTITY', excludeMe, true)
-}
-
-def grabViewsWith(String includeMe='') {
-  grabThings(model,'VIEW', includeMe)
 }
 
 def grabViews() {
   grabThings(model,'VIEW')
 }
 
-def grabViewsWithout(String excludeMe='') {
+def grabViewsWith(includeMe='') {
+  grabThings(model,'VIEW', includeMe)
+}
+
+def grabViewWith(String includeMe='') {
+  grabSingleThing(model,'VIEW', includeMe)
+}
+
+def grabViewsWithout(excludeMe='') {
   grabThings(model,'VIEW', excludeMe, true)
 }
+
+def grabEntities() {
+  grabThings(model,'ENTITY')
+}
+
+def grabEntitiesWith(includeMe='') {
+  grabThings(model,'ENTITY', includeMe)
+}
+
+def grabEntityWith(includeMe='') {
+  grabSingleThing(model,'ENTITY', includeMe)
+}
+
+def grabEntitiesWithout(excludeMe='') {
+  grabThings(model,'ENTITY', excludeMe, true)
+}
+
 def grabAttributes(entity) {
   grabAttributesWith(entity)
 }
 
-def grabAttributesWith(entity, String includeMe='') {
+def grabAttributesWith(entity, includeMe='') {
   grabThings(entity, 'ATTRIBUTE', includeMe)
 }
 
-def grabAttributesWithout(entity, String excludeMe='') {
+def grabAttributeWith(entity, includeMe='') {
+  grabSingleThing(entity, 'ATTRIBUTE', includeMe)
+}
+
+def grabAttributesWithout(entity, excludeMe='') {
   grabThings(entity, 'ATTRIBUTE', excludeMe, true)
 }
 
@@ -97,12 +116,15 @@ def grabIndexes(table) {
 	grabIndexesWith(table)
 }
 
-def grabIndexesWith(table, String includeMe='') {
+def grabIndexesWith(table, includeMe='') {
   grabThings(table,'INDEX', includeMe)
 }
 
+def grabIndexWith(table, includeMe='') {
+  grabSingleThing(table,'INDEX', includeMe)
+}
 
-def grabIndexesWithout(table, String excludeMe='') {
+def grabIndexesWithout(table, excludeMe='') {
   grabThings(table,'INDEX', excludeMe, true)
 }
 
@@ -140,10 +162,23 @@ def logIt(String logItem) {
 def nameContains (needleList, haystack) {
   /*
   returns true if haystack contains any of the strings in the needleList.
+  Each needle uses a double wildcard match by default. Adding a '*' or '%' to the beginning or end
+  of the string will match "ends with" and "begins with" respectivly.
+*
   */
-  retVal = false
+  def retVal = false
+  def wildCards = ['*', '%']
   needleList.each { needle ->
-    retVal = retVal || haystack?.toUpperCase().contains (needle?.trim().toUpperCase())
+    if (wildCards.contains(needle[0])) {
+      // wild card in fist position
+      retVal = retVal || haystack?.toUpperCase().endsWith (needle[1..-1]?.trim().toUpperCase())
+    } else if (wildCards.contains(needle[-1])) {
+      // wild card in last position
+      retVal = retVal || haystack?.toUpperCase().startsWith (needle[0..-2]?.trim().toUpperCase())
+    } else {
+      // double wildcard
+      retVal = retVal || haystack?.toUpperCase().contains (needle?.trim().toUpperCase())
+    }
   }
   return retVal
 }
@@ -169,40 +204,37 @@ def grabTheThings(container, String elementType) {
 	}
 }
 
-def grabThings(grabFrom, String whatToGrab, String includeMe='', Boolean exclude=false) {
+def grabThings(grabFrom, String whatToGrab,  includeMe='', Boolean exclude=false) {
   def retVal = []
   def withList = []
   def thingList = grabTheThings(grabFrom, whatToGrab)
 
+
   if (includeMe) {
-    if (includeMe.contains(',')) {
-       // break the comma seperated string into a list
-       withList = includeMe.split(',')
-       thingList.each { thing ->
-         if (exclude) {
-           if (!nameContains (withList, thing.name)) {
-             retVal << thing
-           }
-         } else {
-           if (nameContains (withList, thing.name)) {
-             retVal << thing
-           }
-         }
-       }
-     } else { // a single value
-       thingList.each { thing ->
-         if (exclude) {
-           if (!thing.name?.toUpperCase().contains(includeMe?.toUpperCase())) {
-             retVal << thing
-           }
-         } else {
-           if (thing.name?.toUpperCase().contains(includeMe?.toUpperCase())) {
-             retVal << thing
-           }
-         }
-       }
-     }
+    // if it is a list just use it
+    if (includeMe.class.toString() == 'class java.util.ArrayList') {
+      withList = includeMe
+    } else {
+      if (includeMe.contains(',')) {
+         // break the comma seperated string into a list
+         withList = includeMe.split(',')
+      } else {  // single value put it in a list
+          withList = [includeMe]
+      }
+    }
+    thingList.each { thing ->
+      if (exclude) {
+        if (!nameContains (withList, thing.name)) {
+          retVal << thing
+        }
+      } else {
+        if (nameContains (withList, thing.name)) {
+          retVal << thing
+        }
+      }
+    }
    } else {  // if (includeMe)
+     // return everything
      retVal = thingList
    }
    return retVal
